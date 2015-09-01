8a3cc1696a7880edcdba1beb3c48ceffd4237d56
Anna Colbert <annascolbert@yahoo.com>
2015-09-01 09:50:26 -0600

    Variables and function script

---
+++ scripting1600/Assets/VariablesAndFunctions.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VariablesAndFunctions : MonoBehaviour
+{   
+	int myInt = 123;
+	//this is a comment
+	void Start ()
+	{
+		Debug.Log (myInt);
+	}
+
+}
