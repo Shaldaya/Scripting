02098824ff213226c86621e989f3b94775fa6c9c
Anna Colbert <annascolbert@yahoo.com>
2015-09-03 09:45:06 -0600

    9/3/15

---
+++ scripting1600/Assets/PassOff.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using System.Collections;
+
+public class PassOff : MonoBehaviour
+{
+	int firstInt = 7;
+	// Use this for initialization
+	void Start ()
+	{
+		Debug.Log (firstInt - 2);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
+
