bdf67e49879d5b1e2d85d1d1621b4f5268e3f56b
Anna Colbert <annascolbert@yahoo.com>
2015-09-24 09:44:23 -0600

    Conventions and syntax

---
+++ scripting1600/Assets/ConventionsAndSyntax.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicSyntax : MonoBehaviour
+{
+	void Start ()
+	{
+		Debug.Log(transform.position.x);
+
+		if(transform.position.y <= 250f)
+		{
+			Debug.Log ("wow!");
+		}
+	}
+}
+//Dot operator, semicolon- when its needed, Curley brackets- indenting, Comments- mulit line
---
+++ scripting1600/Assets/ConventionsAndSyntaxScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+public class ConventionsAndSyntaxScript : MonoBehaviour
+
+{ 
+	int cube = 7;
+	int SplatterWarning = 10;	
+
+		//This is a single line comment.
+			
+			/* This is a multiple line comment or block 
+				of code that needs to be commented out.
+			*/
+	
+	void Update()
+	{
+		//The following line of code is used to track the position on the falling cube. Enter the correct convention.
+			Debug.Log (transform.position);
+		
+		/*The following statement will tell unity when to print "I'm going to splater!" to the console if the cube is < or = 10. 
+			Enter the correct statement.
+		*/
+
+		if (cube < SplatterWarning) 
+		{
+			Debug.Log ("I'm going to splater!");
+		} 
+		else if (cube == SplatterWarning)
+		{
+			Debug.Log ("I'm going to splatter!");
+		}
+	
+	}
+}
